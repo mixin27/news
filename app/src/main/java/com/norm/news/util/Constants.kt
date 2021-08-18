b16aec4a60ba6ffe5098eebf2149f1d00b3d9556
Zayar Tun <kyawzayartun.dev@gmail.com>
2021-08-19 01:03:28 +0630

    remove api key for security and update .gitignore

--- app/src/main/java/com/norm/news/util/Constants.kt
+++ app/src/main/java/com/norm/news/util/Constants.kt
@@ -1,12 +1,14 @@
 package com.norm.news.util
 
+import com.norm.news.BuildConfig
+
 /**
  * Created by Kyaw Zayar Tun on 8/17/21.
  */
 class Constants {
     companion object {
         const val BASE_URL = "https://newsapi.org/v2/"
-        const val API_KEY = "94127380fdfe4b268916724a8da21e63"
+        const val API_KEY = BuildConfig.API_KEY
 
         // Database
         const val DB_NAME = "news_db"
